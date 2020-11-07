80100c73537099c32de856acfd97b876e6a90992
Ben Brumby <bbrumby00@gmail.com>
2020-11-06 21:39:15 -0500

    fixed a typo

--- employee_directory/src/components/DataArea.js
+++ employee_directory/src/components/DataArea.js
@@ -1,2 +1,11 @@
 import React from "react";
 import Nav from "./components/Nav";
+import "../styles/DataArea.css";
+
+function DataArea() {
+    return(
+        <Nav />
+    )
+}
+
+export default DataArea;
